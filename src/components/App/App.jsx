3dab02f8b0fc245ac275850767818a4ea8adef32
Pranay Dadmal <pranaydadmal29@gmail.com>
2021-06-14 16:16:27 +0530

    firebase setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,20 @@
-export const App = () => <>Hello from Pranay</>;
+import { fBase } from 'service';
+import { useEffect } from 'react';
+
+export const App = () => {
+  useEffect(() => {
+    fBase.firestore
+      .collection('chatUsers')
+      .where('userName', '==', 'Pranay')
+      .get()
+      .then(res => {
+        const user = res.docs[0]?.data();
+        console.log(user);
+      });
+  }, []);
+  return (
+    <>
+      <h1>let's start pranay</h1>
+    </>
+  );
+};
